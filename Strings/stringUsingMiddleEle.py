5bccd1d5bf3534b7ec7384ebd2d14b21ad85715e
Harivsrd <harivsrdesineedi@gmail.com>
2026-06-17 12:06:27 +0530

    3 middle elements

--- Strings/stringUsingMiddleEle.py
+++ Strings/stringUsingMiddleEle.py
@@ -1,1 +1,12 @@
+#Create a new String with 3 middle elements
 str = input("Enter string :")
+
+n = len(str) 
+if n % 2 == 0:
+    mid = n//2-1
+else:
+    mid = n//2 
+
+newStr = str[mid-1] + str[mid] + str[mid+1]
+
+print(newStr)
