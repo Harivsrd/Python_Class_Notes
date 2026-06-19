adb174d83675b7ec5759a1ffdf322ce61bb0e577
Harivsrd <harivsrdesineedi@gmail.com>
2026-06-19 14:41:30 +0530

    Multi level inhertnace

---
+++ OOP/Inheritance/Student.py
@@ -0,0 +1,61 @@
+class Student :
+    def __init__(self, rollno, name, branch, section):
+        self._rollno = rollno
+        self._name = name 
+        self._branch = branch 
+        self._section = section 
+    
+    def printDetails(self):
+        print("Student Details :")
+        print("------------------------")
+        print("Roll no :",self._rollno)
+        print("Name :",self._name)
+        print("Branch :",self._branch)
+        print("Section :", self._section)
+        
+class Marks(Student):
+    def __init__(self,rollno, name, branch, section, m1, m2, m3):
+        super().__init__(rollno, name, branch, section)
+        self._m1 = m1 
+        self._m2 = m2 
+        self._m3 = m3 
+        
+    def printMarks(self):
+        print("Marks of subject :")
+        print("----------------------")
+        print("Subject 1 :", self._m1)
+        print("Subject 2 :", self._m2)
+        print("Subject 3 :", self._m3)
+        
+class MarksReport(Marks):
+    def __init__(self, rollno, name, branch, section, m1, m2, m3):
+        super().__init__(rollno, name, branch, section, m1, m2, m3)
+        
+    def total(self):
+        tot = self._m1 + self._m2 + self._m3 
+        return tot 
+    
+    def avg(self):
+        return self.total()/3
+    
+    def grade(self):
+        tot = self.total() 
+        if tot > 290:
+            return 'A'
+        elif tot > 250:
+            return 'B'
+        else:
+            return 'C'
+
+rollno = int(input("Enter roll no: "))
+name = input("Enter name :")        
+branch = input("Enter branch :")
+section = input("Enter section :")
+m1 , m2, m3 = map(int, input("Enter marks for 3 subjects :").split())
+a = MarksReport(rollno, name, branch, section, m1, m2, m3)
+
+a.printDetails()
+a.printMarks()
+print("----------------------------")
+print("Total marks :",a.total(),"\nAverage marks :",a.avg(),"\nGrade :",a.grade())
+        
